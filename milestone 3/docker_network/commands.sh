e6e75ea77a36ec9b38cf099e9c64e7c13b07c3fc
Wágner Bánk <79162540+bankwagner@users.noreply.github.com>
2023-12-10 22:43:18 +0100

    Delete milestone 3/docker_network directory

--- milestone 3/docker_network/commands.sh
+++
@@ -1,3 +0,0 @@
-gdown https://drive.google.com/file/d/1J8hIRo-GT6XrkWemIum8DjRisjff9lca/edit -O unet.h5 --fuzzy
-gdown https://drive.google.com/file/d/1_C-tAH4wee_fkPdO8INrvZ2IGpDW8xh0/edit -O fpn.h5 --fuzzy
-gdown https://drive.google.com/file/d/1H0NnKsYLr8l5o1Xe5vfUiZj4Y6STWUKB/edit -O linknet.h5 --fuzzy
